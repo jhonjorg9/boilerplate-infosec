67686caa84f9dd5e19a7d0cc87facd36139a1174
jhonjorg9 <69646720+jhonjorg9@users.noreply.github.com>
2021-09-14 00:18:16 +0300

    test

--- myApp.js
+++ myApp.js
@@ -1,5 +1,13 @@
 const express = require('express');
+const helmet = require("helmet");
 const app = express();
+
+app.use(helmet.hidePoweredBy());
+app.use(
+  helmet.frameguard({
+    action: "deny",
+  })
+);
 
 
 
